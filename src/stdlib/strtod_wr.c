d4a54b8f6bbf6321647922da1dc50eea76801f1f
Yudai Takada <t.yudai92@gmail.com>
2018-02-05 23:27:08 +0900

    fix: error handling when number not found in string

--- src/stdlib/strtod_wr.c
+++ src/stdlib/strtod_wr.c
@@ -4,7 +4,9 @@
 #define E_SUCCESS    (0)         /* Success */
 #define E_FATAL      (-1)        /* Fatal error */
 #define E_RANGE      (ERANGE)    /* Math result not representable */
+#define E_BADMSG     (EBADMSG)   /* Not a data message */
 #define E_INVAL      (EINVAL)    /* Invalid argument */
+
 
 int
 strtod_wr(const char* nptr,
@@ -15,25 +17,30 @@ strtod_wr(const char* nptr,
     char*  tmpend = NULL;
 
     if ((NULL == nptr) || (NULL == dest)) {
-        return E_INVAL;
+        return (E_INVAL);
     }
 
     errno = 0;
     tmpval = strtod(nptr, &tmpend);
 
-    if (ERANGE == errno) {
-        *dest = tmpval;
-        return E_RANGE;
-    }
-
     if (endptr != NULL) {
         endptr = &tmpend;
     }
 
-    if (nptr == tmpend) {
-        return E_INVAL;
+    if (0 != errno) {
+        if (ERANGE == errno) {
+            *dest = tmpval;
+            return (E_RANGE);
+        }
+    }
+    else {
+        return E_FATAL;
+    }
+
+    if (tmpend == nptr) {
+        return (E_BADMSG);
     }
 
     *dest = tmpval;
-    return E_SUCCESS;
+    return (E_SUCCESS);
 }
